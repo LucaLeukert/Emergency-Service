5f866f4b4abff5ba9aa8ecc7e8834ddbb7af79bd
Luca Leukert <derluleu@gmail.com>
2022-03-03 14:56:29 +0100

    div is now overflow-hidden

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-import './styles/App.css'
-import HomeScreen from './components/HomeScreen/HomeScreen'
 import Login from './components/Login/Login'
 import { useState } from 'react'
+import HomeScreen from './components/HomeScreen'
 
 function App() {
     const [token, setToken] = useState('')
 
     return (
-        <div className='App'>
+        <div className='overflow-hidden'>
             {token ? <HomeScreen /> : <Login setToken={setToken} />}
         </div>
     )
