f7f617a0fd3bd98e744727641c8a19ef01b968c6
Luca Leukert <derluleu@gmail.com>
2022-03-04 15:37:29 +0100

    added ToastContainer and user auth component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,35 @@
 import Login from './components/Login/Login'
-import { useState } from 'react'
 import HomeScreen from './components/HomeScreen'
+import useAuth from './hook/useAuth'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
+import { AuthService } from './auth/AuthService'
 
 function App() {
-    const [token, setToken] = useState('')
-
+    console.log(AuthService.getUser())
     return (
-        <div className='overflow-hidden'>
-            {token ? <HomeScreen /> : <Login setToken={setToken} />}
-        </div>
+        <>
+            <div className='overflow-hidden'>
+                {AuthService.getUser() ? <HomeScreen /> : <Login />}
+                {AuthService.getUser() ? (
+                    <button
+                        className='fixed top-0 rounded bg-gray-200 p-3 right-0 m-3 font-bold shadow-sm hover:shadow-md hover:bg-gray-300 transition-all'
+                        onClick={() => AuthService.logout()}
+                    >
+                        Logout
+                    </button>
+                ) : (
+                    <></>
+                )}
+            </div>
+            <ToastContainer
+                position='top-right'
+                autoClose={2000}
+                hideProgressBar={false}
+                closeOnClick={true}
+                pauseOnHover={false}
+            />
+        </>
     )
 }
 
