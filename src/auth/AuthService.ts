031af65e4afb26058d9e3df2bcc6e3dec52bb3f1
Luca Leukert <derluleu@gmail.com>
2022-03-04 15:34:58 +0100

    implemented basic user auth

--- src/auth/AuthService.ts
+++ src/auth/AuthService.ts
@@ -1,37 +1,35 @@
-import axios from 'axios'
+import axios, { AxiosResponse } from 'axios'
 import { User } from '../interfaces/User'
 
 const apiRoute = 'http://localhost:8080/api/auth'
 
 class AuthService {
-    static async login({
-        username,
-        password,
-    }: {
-        username: string
-        password: string
-    }): Promise<User> {
-        const response = await axios.post(`${apiRoute}/login`, {
-            username,
-            password,
-        })
-        if (response.data.accessToken) {
-            sessionStorage.setItem('user', JSON.stringify(response.data))
-        }
-        return response.data as User
+    static async login(username: string, password: string): Promise<User> {
+        return axios
+            .post(`${apiRoute}/login`, {
+                username,
+                password,
+            })
+            .then((response: AxiosResponse<User>) => {
+                if (response.data.accessToken) {
+                    localStorage.setItem('user', JSON.stringify(response.data))
+                }
+                return response.data
+            })
     }
 
     static logout(): void {
-        sessionStorage.removeItem('user')
+        localStorage.removeItem('user')
+        window.location.reload()
     }
 
-    static getUser(): string | null {
-        return JSON.parse(sessionStorage.getItem('user') as string)
+    static getUser(): User | null {
+        return JSON.parse(localStorage.getItem('user') as string) as User
     }
 }
 
 function authHeader(): { Authorization: string } | {} {
-    const user = JSON.parse(sessionStorage.getItem('user') as string)
+    const user = JSON.parse(localStorage.getItem('user') as string)
     if (user && user.accessToken) {
         return { Authorization: 'Bearer ' + user.accessToken }
     } else {
--- src/components/Login/Login.tsx
+++ src/components/Login/Login.tsx
@@ -1,66 +1,73 @@
-import { FormEvent, useState } from 'react'
+import { useState } from 'react'
+import { toast } from 'react-toastify'
 import { AuthService } from '../../auth/AuthService'
 
-async function loginUser({
-    username,
-    password,
-}: {
-    username: string
-    password: string
-}) {
-    AuthService.login({username, password}).
-    return fetch('http://localhost:8080/login', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: JSON.stringify(username + ':' + password),
-    }).then((data) => data.json())
-}
-
-export default function Login({
-    setToken,
-}: {
-    setToken: (token: string) => void
-}) {
+export default function Login() {
     const [username, setUserName] = useState('')
     const [password, setPassword] = useState('')
 
-    const handleSubmit = async (event: FormEvent) => {
-        const token = await loginUser({
-            username,
-            password,
-        })
-        setToken(token)
+    const handleLogin = async () => {
+        if (username !== '' || password !== '') {
+            toast.promise(
+                AuthService.login(username, password),
+                {
+                    pending: 'Logging in...',
+                    success: {
+                        render() {
+                            window.location.reload()
+
+                            return (
+                                <p>Logged in successfully with {username}.</p>
+                            )
+                        },
+                    },
+                    error: {
+                        render(error: any) {
+                            return (
+                                <p>
+                                    Fehler beim Einloggen: {error.data.message}
+                                </p>
+                            )
+                        },
+                    },
+                },
+                { className: 'select-none' }
+            )
+        } else {
+            toast.error(
+                'Es muss ein Benutzername und Passwort angegeben sein.',
+                { className: 'select-none' }
+            )
+        }
     }
 
     return (
         <>
-            <h1 className='mt-2 ml-2 select-none text-gray-600 text-sm font-bold mb-2'>
+            <h1 className='pt-2 pl-2 select-none bg-gray-50 text-gray-600 text-sm font-bold pb-2'>
                 Emergency Service
             </h1>
-            <div className='bg-white shadow-md rounded p-8 mb-4 flex flex-col items-center'>
-                <div className='mb-6'>
+            <div className='bg-gray-50 shadow-md rounded-b p-8 mb-4 flex flex-col items-center'>
+                <div className='mb-6 w-2/3 max-w-4xl'>
                     <label className='block select-none text-gray-600 text-sm font-bold mb-2'>
                         Username
                     </label>
                     <input
                         type='text'
                         onChange={(e) => setUserName(e.target.value)}
-                        className='shadow appearance-none border rounded border-red w-full py-2 px-3 text-gray-600 mb-3'
+                        className='shadow-sm hover:shadow-md transition-shadow appearance-none border rounded border-red w-full py-2 px-3 text-gray-600 bg-gray-200 mb-3'
                     />
                 </div>
-                <div className='mb-6'>
+                <div className='mb-6 w-2/3 max-w-4xl'>
                     <label className='block select-none text-gray-600 text-sm font-bold mb-2'>
                         Passwort
                     </label>
                     <input
                         type='password'
                         onChange={(e) => setPassword(e.target.value)}
-                        className='shadow appearance-none border rounded border-red w-full py-2 px-3 text-gray-600 mb-1'
+                        className='shadow-sm hover:shadow-md transition-shadow appearance-none border rounded border-red w-full py-2 px-3 text-gray-600 bg-gray-200 mb-1'
                     />
                     {!password ? (
-                        <p className='text-red-600 select-none text-xs italic text-center'>
+                        <p className='text-red-600 select-none text-xs italic text-center mt-1'>
                             Bitte wählen sie ein sicheres Passwort.
                         </p>
                     ) : (
@@ -71,9 +78,9 @@ export default function Login({
                     <button
                         className='bg-blue-600 hover:bg-blue-800 text-white font-bold py-2 px-4 rounded transition-colors'
                         type='submit'
-                        onClick={handleSubmit}
+                        onClick={handleLogin}
                     >
-                        Submit
+                        Einloggen
                     </button>
                 </div>
             </div>
